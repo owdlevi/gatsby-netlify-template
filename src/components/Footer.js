d9c015e1dd3ef82e4b2a2bf7925e8beac909f4fc
David Dozsa <dozsalevi@gmail.com>
2019-07-01 20:24:22 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Footer = class extends React.Component {
+  render() {
+    return (
+      <footer >
+          <div>
+            Footer
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
